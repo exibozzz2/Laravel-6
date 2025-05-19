ccc3f9247f727b69ab9ce20c06f32e4b845a6ddd
exibozzz2 <masaradulovic96@gmail.com>
2025-05-20 01:39:25 +0200

    # Laravel #21 # Create Repositories File Location # Make Products Repository # Insert private variable # Add __construct function connected with Products Model

---
+++ app/Repositories/ProductRepository.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\ProductsModel;
+
+class ProductRepository
+{
+
+    private $productModel;
+
+    public function __construct()
+    {
+        $this->productModel = new ProductsModel();
+    }
+
+}
+?>
