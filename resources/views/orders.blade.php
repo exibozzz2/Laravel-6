8d7de78d928f9ad50a6a54e24cc2ca2ad937012a
exibozzz2 <masaradulovic96@gmail.com>
2025-06-04 05:12:10 +0200

    # Laravel #23 Homework # Export and foreach all data from compact to all orders blade # Foreach inside foreach (cart inside every product) # If product id is same as ordered product id list: # Product id, Unit Price, In Stock, Ordered Amount, Total

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -8,19 +8,28 @@
 
 @section("content")
 
+
+
     <h1 class="text-center">All Orders</h1>
 
-    @foreach($orders as $order)
+    @foreach($allOrders as $order)
+        @foreach($ordersFromSession as $singleOrderedProduct)
+
+         @if($singleOrderedProduct['productId'] == $order->id)
+
         <div class="col-12 d-flex align-items-center justify-content-center">
-
                 <div class="d-flex flex-column bg-white rounded p-3 col-3 m-1">
-                    <p class="bg-danger rounded p-1"><b>Name</b>: {{$order['productName']}}</p>
-                    <p><b>Product ID</b>: {{$order['productId']}}</p>
-                    <p><b>Price</b>: {{$order['productPrice']}}</p>
-                    <p><b>Amount</b>: {{$order['productAmount']}}</p>
+                    <p class="bg-danger rounded p-1"><b>Name</b>: {{$order->name}}</p>
+                    <p><b>Product ID</b>: {{$order->id}}</p>
+                    <p><b>Unit Price</b>: {{$order->price}}</p>
+                    <p><b>In Stock</b>: {{$order->amount}}</p>
+                    <p><b>Ordered</b>: {{$singleOrderedProduct['productAmount']}}pcs</p>
+                    <p><b>Total Price</b>: {{$order->price*$singleOrderedProduct['productAmount']}}</p>
                 </div>
-
         </div>
+
+         @endif
+        @endforeach
     @endforeach
 
 @endsection
